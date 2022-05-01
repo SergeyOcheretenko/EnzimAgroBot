4e4bc6caca7037752d1624172d4292149c61c918
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-05-01 14:07:18 +0300

    feat: create product types keyboard

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ const BOT_TOKEN = CONFIG.bot_token;
 
 const bot = new Telegraf(BOT_TOKEN);
 
+const productTypesKeyboard = Markup.inlineKeyboard([
+    [
+        Markup.button.callback('Інсектициди', 'Insecticides'),
+        Markup.button.callback('Фунгіциди', 'Fungicides')
+    ],
+    [
+        Markup.button.callback('Complex additives', 'ComplexAdditives'),
+        Markup.button.callback('Інокулянти', 'Annoculants')
+    ],
+    [
+        Markup.button.callback('Стимулятори росту', 'GrowthStimulators'),
+        Markup.button.callback('Покращувачі грунту', 'SoilImprovers')
+    ],
+    [
+        Markup.button.callback('Відміна', 'Cancel')
+    ]
+]);
+
 function sendHelp(ctx) {
     ctx.reply('Help for using the system');
 }
