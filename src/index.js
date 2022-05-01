743ccf33a1d22c3597cc6849e60c97f1fff9fc21
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-05-01 11:53:36 +0300

    feat: add initial bot structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('Work');
+import { Telegraf, Composer, Markup, Scenes, session } from 'telegraf';
+import CONFIG from '../CONFIG.json' assert { type: "json" }; 
+
+const BOT_TOKEN = CONFIG.bot_token;
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.command('start', (ctx) => {
+    ctx.reply('Start');
+});
+
+bot.launch();
