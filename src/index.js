31232f31ffedaab9cdaa9035b20f34a2b3e118d6
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-05-01 13:55:16 +0300

    feat: add help function

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ const BOT_TOKEN = CONFIG.bot_token;
 
 const bot = new Telegraf(BOT_TOKEN);
 
+function sendHelp(ctx) {
+    ctx.reply('Help for using the system');
+}
+
 bot.command('start', (ctx) => {
     ctx.reply('Start');
 });
+
+bot.command('help', (ctx) => sendHelp(ctx));
 
 bot.launch();
