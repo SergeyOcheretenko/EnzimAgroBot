a393a88b732caf5b8960fe352eb6621a66da641b
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-05-04 13:35:41 +0300

    fix: add last type of product

--- src/modules/parse-xlsx.js
+++ src/modules/parse-xlsx.js
@@ -47,6 +47,8 @@ export function getXlsxData() {
             }
         }
     }
+    typeAndProducts.products = products;
+    sortByTypeArray.push(typeAndProducts);
 
     return sortByTypeArray;
 }
