1dfcd84a5da1bb7662b9ab5f4e09631adf1f32b1
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-05-05 12:03:43 +0300

    feat: add comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// Імпорт зовнішніх бібліотек
 import { Telegraf, Scenes, session } from 'telegraf';
 
+// Імпорт локальних модулів
 import { parseJSON } from './modules/work-with-json.js';
 import { getAdmins } from './modules/admins-list.js';
 
+// Імпорт Wizard-сцен
 import { checkProductPriceScene } from './modules/check-price.js';
 import { changeDollarScene } from './modules/change-dollar.js';
 
@@ -12,6 +15,8 @@ const CONFIG = parseJSON('CONFIG.json');
 const BOT_TOKEN = CONFIG.bot_token;
 
 const bot = new Telegraf(BOT_TOKEN);
+
+bot.command('start', (ctx) => ctx.reply('Start'));
 
 bot.command('help', (ctx) => ctx.reply('Help for using the system'));
 
@@ -22,12 +27,12 @@ const stage = new Scenes.Stage([
 
 bot.use(session(), stage.middleware());
 
-bot.command('start', (ctx) => ctx.reply('Start'));
-
+// Запуск сцени з цінами продукції Enzim Agro
 bot.command('price', (ctx) => {
     ctx.scene.enter('checkProductPriceScene');
 });
 
+// Запуск сцени зміни курсу USD (лише для адміністраторів)
 bot.command('change_dollar', async (ctx) => {
     const ADMINISTRATORS = getAdmins();
     if (ADMINISTRATORS.includes('@' + ctx.message.from.username)) {
@@ -35,6 +40,7 @@ bot.command('change_dollar', async (ctx) => {
     }
 });
 
+// Отримання поточного курсу USD
 bot.command('dollar_rate', async (ctx) => {
     const dollarRate = await parseJSON('src/json/dollar-rate.json').dollarRate;
     await ctx.reply(`Поточний курс USD: ${dollarRate} грн.`)
