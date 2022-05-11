48dcadf3f896f94a3bb0bfa5ac678856d63b3fdd
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-05-11 11:29:09 +0300

    feat: create Scene constructor

--- src/modules/check-price.js
+++ src/modules/check-price.js
@@ -13,55 +13,59 @@ async function sendCategories(ctx) {
     return;
 }
 
-// Перший крок сцени - надсилання списку категорій
-const startCheckPriceScene = new Composer();
-startCheckPriceScene.on('text', async (ctx) => {
-    sendCategories(ctx);
-    return ctx.wizard.next();
-});
-
-// Другий крок сцени - надсилання списку продуктів обраної категорії
-const selectProduct = new Composer();
-for (const type of keyboards.getTypesList()) {
-    selectProduct.action(type, async (ctx) => {
-        await ctx.reply('Оберіть продукт:', 
-            keyboards.createProductsKeyboards()[type]);
+function createCheckPriceScene() {
+    // Перший крок сцени - надсилання списку категорій
+    const startCheckPriceScene = new Composer();
+    startCheckPriceScene.on('text', async (ctx) => {
+        sendCategories(ctx);
         return ctx.wizard.next();
     });
+
+    // Другий крок сцени - надсилання списку продуктів обраної категорії
+    const selectProduct = new Composer();
+    for (const type of keyboards.getTypesList()) {
+        selectProduct.action(type, async (ctx) => {
+            await ctx.reply('Оберіть продукт:', 
+                keyboards.createProductsKeyboards()[type]);
+            return ctx.wizard.next();
+        });
+    }
+
+    // Третій крок сцени - надсилання ціни обраного продукту
+    const sendPrice = new Composer();
+    sendPrice.action('product1', async (ctx) => {
+        await ctx.reply('*ціна першого продукту*');
+        return ctx.scene.leave();
+    });
+
+    sendPrice.action('product2', async (ctx) => {
+        await ctx.reply('*ціна другого продукту*');
+        return ctx.scene.leave();
+    });
+
+    sendPrice.action('product3', async (ctx) => {
+        await ctx.reply('*ціна третього продукту*');
+        return ctx.scene.leave();
+    });
+
+    sendPrice.action('product4', async (ctx) => {
+        await ctx.reply('*ціна четвертого продукту*');
+        return ctx.scene.leave();
+    });
+
+    sendPrice.action('Back', (ctx) => {
+        sendCategories(ctx);
+        return ctx.wizard.back();
+    });
+
+    const checkPriceScene = new Scenes.WizardScene(
+        'checkPriceScene', 
+        startCheckPriceScene, 
+        selectProduct, 
+        sendPrice
+    );
+
+    return checkPriceScene;
 }
 
-// Третій крок сцени - надсилання ціни обраного продукту
-const sendPrice = new Composer();
-sendPrice.action('product1', async (ctx) => {
-    await ctx.reply('*ціна першого продукту*');
-    return ctx.scene.leave();
-});
-
-sendPrice.action('product2', async (ctx) => {
-    await ctx.reply('*ціна другого продукту*');
-    return ctx.scene.leave();
-});
-
-sendPrice.action('product3', async (ctx) => {
-    await ctx.reply('*ціна третього продукту*');
-    return ctx.scene.leave();
-});
-
-sendPrice.action('product4', async (ctx) => {
-    await ctx.reply('*ціна четвертого продукту*');
-    return ctx.scene.leave();
-});
-
-sendPrice.action('Back', (ctx) => {
-    sendCategories(ctx);
-    return ctx.wizard.back();
-});
-
-const checkProductPriceScene = new Scenes.WizardScene(
-    'checkProductPriceScene', 
-    startCheckPriceScene, 
-    selectProduct, 
-    sendPrice
-);
-
-export { checkProductPriceScene };
+export { createCheckPriceScene };
